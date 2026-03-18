b57ea8f10cfc365eb89e91e510cb9f348c18af1b
Brunomdd <brunnodiniiz@gmail.com>
2026-03-18 10:54:59 -0300

    feat: adiciona integração básica com API e fluxo inicial do CLI

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#Conversor de moedas CLI
+from api import api_moeda
+
+def conversao_taxa(valor,tipo):
+    dados = api_moeda()
+    print(dados)
+
+def main():
+
+    while True:
+        opcao = int(input('escolha uma opção: '))
+        if opcao == 1:
+            valor = float(input('valor: '))
+            tipo = input('digite o tipo de moeda: ')
+            conversao_taxa(valor,tipo)
+
+main()
+
+
+
+
