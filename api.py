f2e70be8314cd3768d9b6d3e1a4ef82238d93c3a
Brunomdd <brunnodiniiz@gmail.com>
2026-03-18 11:48:40 -0300

    feat: melhora API de moedas com tratamento de conexão, Timeout, HTTP e JSON inválido

--- api.py
+++ api.py
@@ -1,5 +1,19 @@
 import requests
 
 def api_moeda():
-    response = requests.get("https://v6.exchangerate-api.com/v6/44d6878ce80641a9ce9ee3eb/latest/USD", timeout=5)
-    return response.json()
+    try:
+        response = requests.get("https://v6.exchangerate-api.com/v6/44d6878ce80641a9ce9ee3eb/latest/USD", timeout=5)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.ConnectionError:
+        print('A API não conseguiu se conectar')
+    except requests.exceptions.Timeout:
+        print('Tempo de resposta expirou!')
+    except requests.exceptions.InvalidURL:
+        print('URL inválida!')
+    except requests.exceptions.HTTPError as error:
+        print(f'Erro: {error}')
+    except ValueError:
+        print('formato de dados inválido!')
+        return None
+
