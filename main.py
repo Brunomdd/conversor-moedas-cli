890c4a36b235fdc9f1d078a1365c660756494688
Brunomdd <brunnodiniiz@gmail.com>
2026-03-18 11:39:36 -0300

    feat: adiciona fluxo de conversão de moedas e menu interativo com validação simples

--- main.py
+++ main.py
@@ -1,17 +1,34 @@
 from api import api_moeda
 
-def conversao_taxa(valor,tipo):
+def conversao_taxa(valor,destino):
     dados = api_moeda()
-    print(dados)
+    if destino not in dados['conversion_rates']:
+        print('Esse tipo de moeda não existe!')
+        return
+
+    taxa = valor * (dados['conversion_rates'][destino])
+    return taxa
 
 def main():
-
     while True:
+        print('1 - Converter moeda')
+        print('2 - Sair')
         opcao = int(input('escolha uma opção: '))
         if opcao == 1:
             valor = float(input('valor: '))
-            tipo = input('digite o tipo de moeda: ')
-            conversao_taxa(valor,tipo)
+            destino = input('digite o tipo de moeda: ').strip().upper()
+            if destino is None:
+                print('Erro, não pode deixar vazio!')
+                continue
+            resposta = conversao_taxa(valor,destino)
+            if resposta:
+                print(f"O valor convertido é {resposta:.2f}")
+        elif opcao == 2:
+            print('Saindo do sistema . . .')
+            break
+        else:
+            print('Opção inválida! ')
+
 
 main()
 
