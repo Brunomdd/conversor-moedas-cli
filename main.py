91ac77efe96cde46e5141037175f4e9e9ea4f671
Brunomdd <brunnodiniiz@gmail.com>
2026-03-20 11:20:25 -0300

    feat: histórico JSON + menu opção 3

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from api import api_moeda
-from uteis import leiaint,leiafloat
+from uteis import leiaint,leiafloat,carregar,salvar
 
 def conversao_taxa(valor,origem,destino):
     dados = api_moeda(origem)
@@ -18,7 +18,6 @@ def conversao_taxa(valor,origem,destino):
         print('Esse tipo de moeda não existe!')
         return
 
-
     taxa = valor * (dados['conversion_rates'][destino])
     return taxa
 
@@ -35,11 +34,27 @@ def moedas_disponiveis():
         print(f'{relatorio}')
 
 
+def ver_historico():
+    historico = carregar()
+    if not historico:
+        print('Não há nada para mostrar no historico!')
+        return
+    for item in historico:
+        print()
+        print(f"Moeda origem: {item['valor_origem']}".center(32))
+        print(f"Moeda destino: {item['valor_destino']}".center(32))
+        print(f'valor original: {item['valor_conversão']}'.center(32))
+        print(f"valor convertido: {item['valor_convertido']}".center(32))
+        print()
+
+
 def main():
+    lista = carregar()
     while True:
         print('1 - Converter moeda')
         print('2 - Listar moedas disponiveis')
-        print('3 - Sair')
+        print('3 - Ver historico')
+        print('4 - Sair ')
         opcao = leiaint('escolha uma opção: ')
         if opcao == 1:
             origem = input('Moeda de origem ex (BRL,EU,USD):').strip().upper()
@@ -51,13 +66,17 @@ def main():
             resposta = conversao_taxa(valor,origem,destino)
             if resposta:
                 print(f"O valor convertido é {resposta:.2f} ")
+                lista.append({'Origem':origem,'destino':destino,"valor_conversão":valor,"valor_convertido":resposta})
+                salvar(lista)
         elif opcao == 2:
             moedas_disponiveis()
         elif opcao == 3:
+            ver_historico()
+        elif opcao == 4:
+            print('Saindo do sistema . . . ')
             break
         else:
             print('Opção inválida!')
-
 
 main()
 
