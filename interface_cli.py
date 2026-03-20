b13f81e0f0b8b9d7c58ca26083e6c445e4459778
Brunomdd <brunnodiniiz@gmail.com>
2026-03-20 14:19:33 -0300

    refactor: extrai moedas_disponiveis e ver_historico para o módulo interface_cli

---
+++ interface_cli.py
@@ -0,0 +1,27 @@
+from api import  api_moeda
+from uteis import carregar
+
+def moedas_disponiveis():
+    dados = api_moeda("USD")
+    if not dados:
+        print('A APi não conseguiu se conectar ,tente novamente mais tarde')
+        return
+
+    quantidade = len(dados['conversion_rates'])
+    print(f'Temos ({quantidade}) moedas disponiveis')
+    for relatorio in sorted(dados['conversion_rates']):
+        print(f'{relatorio}')
+
+
+def ver_historico():
+    historico = carregar()
+    if not historico:
+        print('Não há nada para mostrar no historico!')
+        return
+    for item in historico:
+        print()
+        print(f"Moeda origem: {item['moeda_origem']}".center(32))
+        print(f"Moeda destino: {item['moeda_destino']}".center(32))
+        print(f'valor original: {item['valor_conversão']}'.center(32))
+        print(f"valor convertido: {item['valor_convertido']}".center(32))
+        print()
