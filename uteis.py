34f32860fde9eb25face5f26d1ed669291df1327
Brunomdd <brunnodiniiz@gmail.com>
2026-03-20 11:15:25 -0300

    feat: carregar() e salvar() para histórico JSON

--- uteis.py
+++ uteis.py
@@ -1,3 +1,6 @@
+import json
+from json import  JSONDecodeError
+
 def leiaint(msg):
     while True:
         try:
@@ -14,3 +17,20 @@ def leiafloat(msg):
             return valor
         except ValueError:
             print('erro! digite um número float!')
+
+
+from json import  JSONDecodeError
+
+def carregar():
+    lista = []
+    try:
+        with open("moeda_historico.json","r",encoding="utf-8") as arq:
+            return json.load(arq)
+    except (FileNotFoundError,JSONDecodeError):
+        lista = []
+    return lista
+
+def salvar(lista):
+    with open("moeda_historico.json","w",encoding='utf-8') as arq:
+        json.dump(lista,arq,ensure_ascii= False,indent=4)
+
