56cffbbe15a30d4f1fa6693a25334a80bcb6655c
Brunomdd <brunnodiniiz@gmail.com>
2026-03-20 11:44:21 -0300

    feat: opção 4 limpar histórico + padroniza chaves

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ def ver_historico():
         return
     for item in historico:
         print()
-        print(f"Moeda origem: {item['valor_origem']}".center(32))
-        print(f"Moeda destino: {item['valor_destino']}".center(32))
+        print(f"Moeda origem: {item['moeda_origem']}".center(32))
+        print(f"Moeda destino: {item['moeda_destino']}".center(32))
         print(f'valor original: {item['valor_conversão']}'.center(32))
         print(f"valor convertido: {item['valor_convertido']}".center(32))
         print()
@@ -54,7 +54,8 @@ def main():
         print('1 - Converter moeda')
         print('2 - Listar moedas disponiveis')
         print('3 - Ver historico')
-        print('4 - Sair ')
+        print('4 - Limpar Histórico ')
+        print('5 - Sair')
         opcao = leiaint('escolha uma opção: ')
         if opcao == 1:
             origem = input('Moeda de origem ex (BRL,EU,USD):').strip().upper()
@@ -66,15 +67,23 @@ def main():
             resposta = conversao_taxa(valor,origem,destino)
             if resposta:
                 print(f"O valor convertido é {resposta:.2f} ")
-                lista.append({'Origem':origem,'destino':destino,"valor_conversão":valor,"valor_convertido":resposta})
+                lista.append({'moeda_origem':origem,'moeda_destino':destino,"valor_conversão":valor,"valor_convertido":resposta})
                 salvar(lista)
         elif opcao == 2:
             moedas_disponiveis()
         elif opcao == 3:
             ver_historico()
         elif opcao == 4:
-            print('Saindo do sistema . . . ')
+            limpar = carregar()
+            if limpar:
+                print('O historico foi limpo com sucesso! ✅')
+                salvar([])
+            else:
+                print('Não há nada para limpar no historico!')
+        elif opcao == 5:
+            print('Saindo do sistema . . .')
             break
+
         else:
             print('Opção inválida!')
 
