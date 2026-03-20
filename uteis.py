52d5d578e2f46da1723005a2fe85e5201d3fe3cf
Brunomdd <brunnodiniiz@gmail.com>
2026-03-20 14:25:17 -0300

    feat: adiciona função confirmar_acao para validação de entrada do usuário

--- uteis.py
+++ uteis.py
@@ -1,6 +1,13 @@
 import json
 from json import  JSONDecodeError
 
+def confirmar_acao(msg):
+    while True:
+        resp = str(input(msg)).strip().upper()
+        if resp in ('S','N'):
+            return  resp
+
+
 def leiaint(msg):
     while True:
         try:
@@ -24,13 +31,13 @@ from json import  JSONDecodeError
 def carregar():
     lista = []
     try:
-        with open("moeda_historico.json","r",encoding="utf-8") as arq:
+        with open("history.json","r",encoding="utf-8") as arq:
             return json.load(arq)
     except (FileNotFoundError,JSONDecodeError):
         lista = []
     return lista
 
 def salvar(lista):
-    with open("moeda_historico.json","w",encoding='utf-8') as arq:
+    with open("history.json","w",encoding='utf-8') as arq:
         json.dump(lista,arq,ensure_ascii= False,indent=4)
 
