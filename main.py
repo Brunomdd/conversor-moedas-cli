7698934bd071bb1bbe1e74a6f29c200a2d389177
Brunomdd <brunnodiniiz@gmail.com>
2026-03-23 14:04:29 -0300

    refactor: melhorar validações e fluxo no main
    - validar entrada de origem e destino
    - tratar retorno None da conversão
    - ajustar saída da conversão
    - melhorar controle do histórico (limpeza e salvamento)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from uteis import leiaint,leiafloat,carregar,salvar,confirmar_acao
 from conversor import conversao_taxa
-from listar_moedas import moedas_disponiveis,ver_historico
+from interface_cli import moedas_disponiveis,ver_historico
 
 def main():
     lista = carregar()
@@ -12,31 +12,34 @@ def main():
         print('5 - Sair')
         opcao = leiaint('escolha uma opção: ')
         if opcao == 1:
-            origem = input('Moeda de origem ex (BRL,EU,USD): ').strip().upper()
-            if not origem:
-                print('Erro não pode deixar vazio!')
+            origem = input('Moeda de origem ex (BRL,EUR,USD): ').strip().upper()
+            if not origem or len(origem)!= 3:
+                print('Erro moeda de origem inválida')
                 continue
-            destino = input('Moeda de destino ex (BRL,EU,USD):  ').strip().upper()
-            if not origem or not destino:
-                print('Erro! o campo não foi preenchido.')
+            destino = input('Moeda de destino ex (BRL,EUR,USD):  ').strip().upper()
+            if not destino or len(destino) != 3:
+                print(f'Erro, moeda de destino inválida')
                 continue
+
             valor = leiafloat('valor: ')
             if valor <= 0:
                 print('Digite um número maior do que 0.')
                 continue
 
             resposta = conversao_taxa(valor,origem,destino)
-            print(f"{valor} {origem} → {resposta:.2f} {destino}")
-            lista.append({'moeda_origem':origem,'moeda_destino':destino,"valor_conversão":valor,"valor_convertido":resposta})
-            salvar(lista)
+            if resposta is None:
+                print('Erro ao converter a moeda')
+            else:
+                print(f"{valor} {origem} → {resposta} {destino}")
+                lista.append({'moeda_origem':origem,'moeda_destino':destino,"valor_conversão":valor,"valor_convertido":resposta})
+                salvar(lista)
 
         elif opcao == 2:
             moedas_disponiveis()
         elif opcao == 3:
             ver_historico()
         elif opcao == 4:
-            limpar = carregar()
-            if limpar:
+            if lista:
                 resp = confirmar_acao("Quer continuar? [S/N]")
                 if resp == 'N':
                     print('Manteremos o estado atual do seu arquivo.')
@@ -46,6 +49,7 @@ def main():
             else:
                 print('Não há nada para limpar no historico!❌')
         elif opcao == 5:
+            salvar(lista)
             print('Saindo do sistema . . .')
             break
         else:
